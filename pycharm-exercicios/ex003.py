243c7ce0783fd2aca612ba4a9d88baf1061ba991
Rafael-Mowses <rafaelmowsestec@gmail.com>
2022-01-18 15:14:32 -0300

    Att calculadora

--- pycharm-exercicios/ex003.py
+++ pycharm-exercicios/ex003.py
@@ -1,5 +1,5 @@
 print('========CALCULADORA DE ADIÇÃO========')
-n1 =int(input('Digite um número: '))
-n2 =int(input('Digite mais um número: '))
-s = n1+n2
-print('A Soma vale',s)
+n1 = int(input('Digite um Valor: '))
+n2 = int(input('Digite Outro: '))
+s = n1 + n2
+print('A soma entre {} e {} vale {}'.format(n1, n2, s))
